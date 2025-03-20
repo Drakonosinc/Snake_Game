d9c0fd9e572c9f6efefbe4b5d961343bb285ef4c
matias <mat20can@gmail.com>
2025-03-20 16:02:34 -0300

    version 10.0

---
+++ src/Interface/Interface.py
@@ -0,0 +1,6 @@
+from Loaders.Load_elements import *
+from .Elements_interface import *
+class interface(objects):
+    def __init__(self):
+        super().__init__()
+        self.main=0 #-1=game, 0=menu, 1=game over, 2=game menu, 3=pausa, 4=options, 5=visuals, 6=menu keys, 7=sound menu
