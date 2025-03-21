a80b39547fa2dec642f09b42ebdacdc7991abe88
matias <mat20can@gmail.com>
2025-03-21 16:34:20 -0300

    version 10.0

--- src/Loaders/Config_Loader.py
+++ src/Loaders/Config_Loader.py
@@ -2,3 +2,7 @@ import json,os
 from pygame.locals import *
 class Config():
     def __init__(self):self.base_dir = os.path.abspath(os.path.join(__file__, "../../.."))
+    def load_config(self):
+        try:
+            config_path = os.path.join(self.base_dir, "Config")
+        except:self.config(alls=True),self.save_config()
