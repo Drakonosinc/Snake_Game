423222d65d189a34da552dd8eb303aea886c06c9
matias <mat20can@gmail.com>
2025-03-21 16:36:42 -0300

    version 10.0

--- src/Loaders/Load_elements.py
+++ src/Loaders/Load_elements.py
@@ -6,3 +6,5 @@ class load_elements():
     def __init__(self):
         pygame.init()
         pygame.display.set_caption("Snake Game")
+        self.config=Config()
+        self.config.load_config()
