a6618ba9e15ab97e374f129f7b72db0920e2c1b9
matias <mat20can@gmail.com>
2025-03-21 16:34:48 -0300

    version 10.0

--- src/Loaders/Config_Loader.py
+++ src/Loaders/Config_Loader.py
@@ -6,3 +6,6 @@ class Config():
         try:
             config_path = os.path.join(self.base_dir, "Config")
         except:self.config(alls=True),self.save_config()
+    def config(self,visuals=False,keys=False,sounds=False,AI=False,game=False,alls=False):
+        pass
+    
