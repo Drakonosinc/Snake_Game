f09b4228299b1c1d91bbbd1b0f8a3024ff67989d
matias <mat20can@gmail.com>
2025-03-22 17:57:51 -0300

    version 10.0

--- src/Interface/Interface.py
+++ src/Interface/Interface.py
@@ -5,3 +5,13 @@ class interface(load_elements):
         super().__init__()
         self.main=0 #-1=game, 0=menu, 1=game over, 2=game menu, 3=pausa, 4=options, 5=visuals, 6=menu keys, 7=sound menu
         self.mode_game={"Training AI":False,"Player":True,"AI":False}
+    def draw_interfaces(self):
+        self.main_menu()
+        self.menu_options()
+        self.mode_game_menu()
+        self.game_over_menu()
+        self.pausa_menu()
+        self.sounds_menu()
+        self.visuals_menu()
+        self.keys_menu()
+        self.draw_generation()
