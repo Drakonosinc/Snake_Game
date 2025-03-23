64fe46342546da607d51ae2e46b88fd0a7309f47
matias <mat20can@gmail.com>
2025-03-23 15:10:56 -0300

    version 10.0

--- src/Loaders/Load_elements.py
+++ src/Loaders/Load_elements.py
@@ -56,7 +56,7 @@ class load_elements():
     def load_fonts(self):
         self.font=pygame.font.SysFont("times new roman",60)
         self.font_0=pygame.font.SysFont("times new roman",30)
-        self.font=pygame.font.Font(None,25)
+        self.font_0_5=pygame.font.Font(None,25)
         self.font1=pygame.font.SysFont("times new roman", 80)
         self.font2=pygame.font.Font(None,35)
         self.font2_5=pygame.font.Font(os.path.join(self.font_path,"ka1.ttf"),30)
