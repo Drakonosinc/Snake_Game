745bc30c13e57c07afeebd4faac6484b61ebca04
matias <mat20can@gmail.com>
2025-03-24 16:15:19 -0300

    version 10.0

--- src/Entities/Apple.py
+++ src/Entities/Apple.py
@@ -3,3 +3,5 @@ class Apple:
     def __init__(self, x, y, width, height):
         self.rect = Rect(x, y, width, height)
         self.reset_position = (x, y, width, height)
+    def reset(self):
+        self.rect = Rect(*self.reset_position)
