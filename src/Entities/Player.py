9ce660983f950d0b396af885971442da5a3fd2df
matias <mat20can@gmail.com>
2025-03-26 15:49:30 -0300

    version 10.0

--- src/Entities/Player.py
+++ src/Entities/Player.py
@@ -5,15 +5,15 @@ class Player:
         self.reset_position = (x, y, width, height)
         self.move_speed = 3
         self.direction = "RIGHT"
-        self.change_to = self.direction
+        # self.change_to = self.direction
         self.reward = 0
         self.score = 0
         self.active = True
-    def move(self, change):
-        if change == "UP" and self.direction != "DOWN":self.direction = "UP"
-        if change == "DOWN" and self.direction != "UP":self.direction = "DOWN"
-        if change == "LEFT" and self.direction != "RIGHT":self.direction = "LEFT"
-        if change == "RIGHT" and self.direction != "LEFT":self.direction = "RIGHT"
+    # def move(self, change):
+    #     if change == "UP" and self.direction != "DOWN":self.direction = "UP"
+    #     if change == "DOWN" and self.direction != "UP":self.direction = "DOWN"
+    #     if change == "LEFT" and self.direction != "RIGHT":self.direction = "LEFT"
+    #     if change == "RIGHT" and self.direction != "LEFT":self.direction = "RIGHT"
         # if self.direction == "UP":self.rect.y -= self.move_speed
         # if self.direction == "DOWN":self.rect.y += self.move_speed
         # if self.direction == "LEFT":self.rect.x -= self.move_speed
