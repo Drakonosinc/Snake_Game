6fa9e4e0149acf20cd762212769ca025e55596b9
matias <mat20can@gmail.com>
2025-03-28 15:50:39 -0300

    version 10.0

--- src/Entities/Player.py
+++ src/Entities/Player.py
@@ -1,1 +1,13 @@
 from pygame import *
+class Player:
+    def __init__(self, x, y, width=25, height=25, body=[]):
+        self.rect_head = Rect(x, y, width, height)
+        self.body = [Rect(*i, width, height) for i in body]
+        self.reset_head_position = (x, y, width, height)
+        self.reset_body_position = [(*i, width, height) for i in body]
+        self.move_speed = 3
+        self.direction = "RIGHT"
+        self.reward = 0
+        self.score = 0
+        self.active = True
+    
