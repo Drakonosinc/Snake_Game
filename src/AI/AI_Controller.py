2ee1948980b82c243269ae02b56ca5212e95aeb0
matias <mat20can@gmail.com>
2025-03-31 15:40:59 -0300

    version 10.0

--- src/AI/AI_Controller.py
+++ src/AI/AI_Controller.py
@@ -1,10 +1,10 @@
 import torch
 import numpy as np
 class AIController():
-    def __init__(self):
-        pass
+    def __init__(self, game):self.game = game
     def get_state(self):
-        return np.array([])
+        return np.array([self.game.player.rect_head.x, self.game.player.rect_head.y,self.game.fruit.x, self.game.fruit.y,
+                        ])
     def actions_AI(self,model):
         state=self.get_state()
         action = model(torch.tensor(state, dtype=torch.float32)).detach().numpy()
