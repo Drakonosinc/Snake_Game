62a1ed84786aae91ddeb25e4e40eab2ccdadace8
matias <mat20can@gmail.com>
2025-03-31 16:13:58 -0300

    version 10.0

--- src/AI/AI_Controller.py
+++ src/AI/AI_Controller.py
@@ -9,5 +9,13 @@ class AIHandler():
         state=self.get_state()
         action = model(torch.tensor(state, dtype=torch.float32)).detach().numpy()
         self.AI_actions(action)
+    def softmax(self, x):
+        exp_x = np.exp(x - np.max(x))
+        return exp_x / exp_x.sum()
     def AI_actions(self,action):
-        pass
+        probabilities = self.softmax(action)
+        chosen_action = np.argmax(probabilities)
+        if chosen_action == 0:self.game.player.direction = "UP"
+        elif chosen_action == 1:self.game.player
+        elif chosen_action == 2:self.game.player
+        elif chosen_action == 2:self.game.player
