0d543d51976e698b04688e2163fc82df852443d3
matias <mat20can@gmail.com>
2025-04-01 15:34:27 -0300

    version 10.0

--- src/Loaders/Load_elements.py
+++ src/Loaders/Load_elements.py
@@ -14,7 +14,7 @@ class load_elements():
         self.config_screen()
     def load_AI(self):
         self.model_path=os.path.join(self.config.base_dir, "AI/best_model.pth")
-        # self.model_training = load_model(self.model_path, , ) if os.path.exists(self.model_path) else None
+        self.model_training = load_model(self.model_path, 10, 4) if os.path.exists(self.model_path) else None
     def config_screen(self):
         self.WIDTH,self.HEIGHT=600,400
         self.screen=pygame.display.set_mode((self.WIDTH,self.HEIGHT))
