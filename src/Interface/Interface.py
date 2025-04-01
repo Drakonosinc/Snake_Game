aade9adc91c09226724ab921157b64082180370a
matias <mat20can@gmail.com>
2025-04-01 15:27:02 -0300

    version 10.0

--- src/Interface/Interface.py
+++ src/Interface/Interface.py
@@ -77,7 +77,14 @@ class interface(load_elements):
         self.screen.blit(self.font2_5.render(f"Save model", True, "White"),(self.WIDTH/2+40,self.HEIGHT/2+125))
         self.execute_buttons(self.increase_generation,self.decrease_generation,self.increase_population,self.decrease_population,self.increase_try_for_ai,self.decrease_try_for_ai,self.save_model)
         self.save_model.change_item({"color":self.SKYBLUE if self.config.config_AI["model_save"] else self.RED,"text":"ON" if self.config.config_AI["model_save"] else "OFF"})
-    def buttons_config_AI(self):pass
+    def buttons_config_AI(self):
+        self.increase_generation = self.button_factory_f2_5.create_TextButton({"font":self.font3_5,"text": ">","position": (self.WIDTH-140,self.HEIGHT/2-70),"command1":lambda:self.increase_decrease_variable(self.config._AI,'generation_value')})
+        self.decrease_generation = self.button_factory_f2_5.create_TextButton({"font":self.font3_5,"text": "<","position": (self.WIDTH-265,self.HEIGHT/2-70),"command1":lambda:self.increase_decrease_variable(self.config.config_AI,'generation_value',True,-1)})
+        self.increase_population = self.button_factory_f2_5.create_TextButton({"font":self.font3_5,"text": ">","position": (self.WIDTH-140,self.HEIGHT/2+5),"command1":lambda:self.increase_decrease_variable(self.config.config_AI,'population_value')})
+        self.decrease_population = self.button_factory_f2_5.create_TextButton({"font":self.font3_5,"text": "<","position": (self.WIDTH-265,self.HEIGHT/2+5),"command1":lambda:self.increase_decrease_variable(self.config.config_AI,'population_value',True,-1)})
+        self.increase_try_for_ai = self.button_factory_f2_5.create_TextButton({"font":self.font3_5,"text": ">","position": (self.WIDTH-140,self.HEIGHT/2+80),"command1":lambda:self.increase_decrease_variable(self.config.config_AI,'try_for_ai')})
+        self.decrease_try_for_ai = self.button_factory_f2_5.create_TextButton({"font":self.font3_5,"text": "<","position": (self.WIDTH-265,self.HEIGHT/2+80),"command1":lambda:self.increase_decrease_variable(self.config.config_AI,'try_for_ai',True,-1)})
+        self.save_model = self.button_factory_f2_5.create_TextButton({"text": "OFF","color": self.SKYBLUE,"position": (self.WIDTH-105,self.HEIGHT/2+125),"command1":lambda:self.on_off(self.config.config_AI,"model_save"),"command2":self.config.save_config})
     def pausa_menu(self):
         self.screen.fill(self.BLACK)
     def buttons_pausa(self):pass
