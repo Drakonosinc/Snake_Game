77282617015ebcd554ca8d179ef439ac6848656a
matias <mat20can@gmail.com>
2025-04-09 16:23:24 -0300

    version 10.0

--- src/Interface/Elements_interface.py
+++ src/Interface/Elements_interface.py
@@ -15,6 +15,8 @@ class ElementsFactory:
         return PolygonButton({"screen": self.screen,"color": self.color,"hover_color": self.hover_color,"sound_hover": self.sound_hover,"sound_touch": self.sound_touch,**config})
     def create_InputText(self,config:dict):
         return Input_text({"screen": self.screen,"font": self.font,"color": self.color,"color_back":self.color_back,"hover_color": self.hover_color,"sound_hover": self.sound_hover,"sound_touch": self.sound_touch,**config})
+    def create_ScrollBar(self,config:dict):
+        return ScrollBar({"screen": self.screen,"color": self.color,"hover_color": self.hover_color,"sound_hover": self.sound_hover,"sound_touch": self.sound_touch,**config})
 class TextButton:
     def __init__(self,config:dict):
         self.screen = config["screen"]
