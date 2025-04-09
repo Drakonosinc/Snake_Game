2660805b93508ed7dd02f1070049b65e16e087d0
matias <mat20can@gmail.com>
2025-04-09 17:07:56 -0300

    version 10.0

--- src/Interface/Interface.py
+++ src/Interface/Interface.py
@@ -83,12 +83,13 @@ class interface(load_elements):
     def pausa_menu(self):
         self.filt(self.WIDTH,self.HEIGHT,150,self.GRAY)
         self.screen.blit(self.font3.render("Pause", True, "orange"),(int(self.WIDTH * (52 / 600)),int(self.HEIGHT * (20 / 400 ))))
-        self.execute_buttons(self.reset_button,self.option_button,self.menu_button,self.exit_button)
+        self.execute_buttons(*self.buttons_in_pausa)
     def buttons_pausa(self):
         self.reset_button = self.button_factory_f2_5.create_TextButton({"text": "Reset","position": (50,self.HEIGHT/2-100),"command1":self.reset,"command2":lambda:self.change_mains({"main":-1})})
         self.option_button = self.button_factory_f2_5.create_TextButton({"text": "Option","position": (50,self.HEIGHT/2-50),"command1":self.reset,"command2":lambda:self.change_mains({"main":4,"run":True}),"command3":self.check_sounds})
         self.menu_button = self.button_factory_f2_5.create_TextButton({"text": "Menu","position": (50,self.HEIGHT/2),"command1":self.reset,"command2":lambda:self.change_mains({"main":0,"run":True}),"command3":self.check_sounds})
         self.exit_button = self.button_factory_f2_5.create_TextButton({"text": "Exit","position": (50,self.HEIGHT/2+50),"sound_touch": self.sound_exit,"command1":self.close_game})
+        self.buttons_in_pausa=[self.reset_button,self.option_button,self.menu_button,self.exit_button]
     def menu_options(self):
         self.screen.fill(self.BLACK)
         self.screen.blit(self.font3.render("Options", True, "orange"),(int(self.WIDTH * (52 / 600)),int(self.HEIGHT * (20 / 400 ))))
