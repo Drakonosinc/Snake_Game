943e375e270efb6524b714bf8d0577e500a1b2d1
matias <mat20can@gmail.com>
2025-04-16 16:57:41 -0300

    version 10.0

--- src/Interface/Elements_interface.py
+++ src/Interface/Elements_interface.py
@@ -202,6 +202,7 @@ class ScrollBar:
         if self.pressed_keep:self.pressed_keep_button(pygame.mouse.get_pressed(),pygame.mouse.get_pos())
     def mouse_collision(self,mouse_pos):pass
     def pressed_button(self,pressed_mouse,mouse_pos):
+        current_time = pygame.time.get_ticks()
         
     def pressed_keep_button(self,pressed_mouse,mouse_pos):pass
     def execute_commands(self):
