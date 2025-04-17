14dc92a0e91ecd1e1b9e357ddd428eafde9873db
matias <mat20can@gmail.com>
2025-04-17 13:56:48 -0300

    version 10.0

--- src/Interface/Elements_interface.py
+++ src/Interface/Elements_interface.py
@@ -219,3 +219,6 @@ class ScrollBar:
             el.rect.y = new_y
         if callable(self.callback):
             self.callback(proportion)
+    def draw(self):
+        pygame.draw.rect(self.screen, self.color, self.rect)
+        pygame.draw.rect(self.screen, self.color_thumb, self.thumb_rect)
