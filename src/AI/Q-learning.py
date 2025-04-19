2b9244b8dd4c1216b034ebb9e79ffe99edf5affb
matias <mat20can@gmail.com>
2025-04-19 16:28:09 -0300

    version 10.0

---
+++ src/AI/Q-learning.py
@@ -0,0 +1,9 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import random
+import numpy as np
+from collections import deque
+from Game.Snake_Game import Snake_Game
+from AI.AI_Controller import AIHandler
+from AI.Neural_Network import SimpleNN
