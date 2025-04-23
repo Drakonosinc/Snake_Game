3e930bdef7bcb91638720be6da31c94a44249fad
matias <mat20can@gmail.com>
2025-04-23 15:16:09 -0300

    version 10.0

--- src/Interface/Elements_interface.py
+++ src/Interface/Elements_interface.py
@@ -62,7 +62,10 @@ class Text:
     def mouse_collision(self,mouse_pos):
         if self.rect.collidepoint(mouse_pos):
             self.screen.blit(self.font.render(self.text,True,self.hover_color),self.position)
-            
+            if self.states["detect_hover"]:
+                if self.sound_hover:self.sound_hover.play(loops=0)
+                self.states["detect_hover"]=False
+        else:self.states["detect_hover"]=True
 class TextButton:
     def __init__(self,config:dict):
         self.screen = config["screen"]
