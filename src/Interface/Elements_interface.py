8db407673cf1043bf5bd9d7c804bdc1f30a2311d
matias <mat20can@gmail.com>
2025-04-23 15:13:25 -0300

    version 10.0

--- src/Interface/Elements_interface.py
+++ src/Interface/Elements_interface.py
@@ -25,6 +25,10 @@ class ElementBehavior:
         self.states=config.get("states",{"detect_hover":True,"presses_touch":True,"click_time": None,"active":False})
         self.commands = [config.get(f"command{i}") for i in range(1,4)]
         self.new_events(time=config.get("time",500))
+    def events(self, event):pass
+    def new_events(self,time):
+        self.EVENT_NEW = pygame.USEREVENT + 1
+        pygame.time.set_timer(self.EVENT_NEW,time)
     
 class TextButton:
     def __init__(self,config:dict):
