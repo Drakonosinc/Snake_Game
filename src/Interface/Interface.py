9aeb728bc7eb5f586552237632d68c1338598bfa
matias <mat20can@gmail.com>
2025-04-30 15:30:46 -0300

    version 10.0

--- src/Interface/Interface.py
+++ src/Interface/Interface.py
@@ -184,6 +184,8 @@ class interface(load_elements):
             self.text_in_training_ai=[self.text_C,self.text_G,self.text_P,self.text_A,self.text_S]
         else:
             self.text_G.change_item({"text": f"Generation Size\n{self.config.config_AI['generation_value']:^36}"})
+            self.text_P.change_item({"text": f"Population Size\n{self.config.config_AI['population_value']:^36}"})
+            
     def buttons_config_AI(self):
         self.increase_generation = self.button_factory_f2_5.create_TextButton({"font":self.font3_5,"text": ">","position": (300,self.HEIGHT/2-95),"command1":lambda:self.increase_decrease_variable(self.config.config_AI,'generation_value')})
         self.decrease_generation = self.button_factory_f2_5.create_TextButton({"font":self.font3_5,"text": "<","position": (120,self.HEIGHT/2-95),"command1":lambda:self.increase_decrease_variable(self.config.config_AI,'generation_value',True,-1)})
