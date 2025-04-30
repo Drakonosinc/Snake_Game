b46d7c8578d15b0eab41e349406950a706d9530b
matias <mat20can@gmail.com>
2025-04-30 15:28:29 -0300

    version 10.0

--- src/Interface/Interface.py
+++ src/Interface/Interface.py
@@ -171,7 +171,7 @@ class interface(load_elements):
         self.config.save_config()
     def menu_AI(self):
         self.screen.fill(self.BLACK)
-        self.execute_buttons(*self.buttons_in_config_AI,self.scroll)
+        self.execute_buttons(*self.buttons_in_config_AI,self.scroll,*self.text_in_training_ai)
         self.save_model.change_item({"color":self.SKYBLUE if self.config.config_AI["model_save"] else self.RED,"text":"ON" if self.config.config_AI["model_save"] else "OFF"})
         self.scroll.update_elements([*self.buttons_in_config_AI[:-2],*self.text_in_training_ai])
     def text_training_ai(self):
@@ -195,6 +195,7 @@ class interface(load_elements):
         self.B_mode_game_button = self.button_factory_f2_5.create_TextButton({"font": self.font1,"text": "←","position": (35,self.HEIGHT-100),"command1":lambda:self.change_mains({"main":2})})
         self.buttons_in_config_AI=[self.increase_generation,self.decrease_generation,self.increase_population,self.decrease_population,self.increase_try_for_ai,self.decrease_try_for_ai,self.save_model,self.continue_button,self.B_mode_game_button]
         self.scroll=self.button_factory_f2_5.create_ScrollBar({"position": (self.WIDTH-30, 50, 20, self.HEIGHT-100),"thumb_height": 20})
+        self.text_training_ai()
     def draw_generation(self):
         if self.main==-1 and self.mode_game["Training AI"]:self.screen.blit(self.font5.render(f"Generation: {int(self.generation)}", True, "orange"),(0,25))
     def show_score(self):
