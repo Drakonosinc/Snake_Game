466dc46a1f9c8f91dc747b2c51cceded142999c5
matias <mat20can@gmail.com>
2025-06-21 16:06:06 -0300

    version 10.0

---
+++ src/Interface/Menus/Base_Menu.py
@@ -0,0 +1,8 @@
+class BaseMenu:
+    def __init__(self, interface=None):
+        self.interface = interface
+        if interface:
+            self.screen = interface.screen
+            self.WIDTH = interface.WIDTH
+            self.HEIGHT = interface.HEIGHT
+            self.config = interface.config
