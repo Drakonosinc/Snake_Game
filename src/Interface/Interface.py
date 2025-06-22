717f24d3cba294b94cb188874bc37de381d3430e
matias <mat20can@gmail.com>
2025-06-22 15:44:33 -0300

    version 10.0

--- src/Interface/Interface.py
+++ src/Interface/Interface.py
@@ -196,16 +196,7 @@ class interface(load_elements,BaseMenu):
         if self.main==-1 and self.mode_game["Training AI"]:self.screen.blit(self.font5.render(f"Generation: {int(self.generation)}", True, "orange"),(0,25))
     def show_score(self):
         if self.main==-1 or self.main==1:self.screen.blit(self.font5.render(f"Score: {int(self.player.score)}", True, "orange"),(0,0))
-    def fade_transition(self,fade_in,color=(0,0,0),limit=255):
-        overlay = pygame.Surface((self.WIDTH, self.HEIGHT))
-        overlay.fill(color)
-        alpha=0 if not fade_in else 255
-        while (not fade_in and alpha <= limit) or (fade_in and alpha >= limit):
-            overlay.set_alpha(alpha)
-            self.screen.blit(overlay, (0, 0))
-            pygame.display.flip()
-            self.clock.tick(20)
-            alpha += -15 if fade_in else 15
+    
     def change_mains(self,config):
         if fade_in:=config.get("fade_in",True):self.fade_transition(False,config.get("color",(0,0,0)),config.get("limit",255))
         if fade_out:=config.get("fade_out",False):self.fade_transition(True,config.get("color2",(0,0,0)),0)
--- src/Interface/Menus/Base_Menu.py
+++ src/Interface/Menus/Base_Menu.py
@@ -26,3 +26,13 @@ class BaseMenu:
         elif dic!=None:dic[variable]+=number
         else:setattr(self,variable,getattr(self,variable)+number)
         if save:self.config.save_config()
+    def fade_transition(self,fade_in,color=(0,0,0),limit=255):
+        overlay = pygame.Surface((self.WIDTH, self.HEIGHT))
+        overlay.fill(color)
+        alpha=0 if not fade_in else 255
+        while (not fade_in and alpha <= limit) or (fade_in and alpha >= limit):
+            overlay.set_alpha(alpha)
+            self.screen.blit(overlay, (0, 0))
+            pygame.display.flip()
+            self.clock.tick(20)
+            alpha += -15 if fade_in else 15
