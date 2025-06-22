756ef5381ecdf9fc5bbccdd1c24dacb38020b445
matias <mat20can@gmail.com>
2025-06-22 16:01:11 -0300

    version 10.0

--- src/Interface/Menus/Main_Menu.py
+++ src/Interface/Menus/Main_Menu.py
@@ -3,5 +3,6 @@ class MainMenu(BaseMenu):
     def __init__(self, interface):
         super().__init__(interface)
         self.buttons = {}
-    def setup_buttons(self):pass
+    def setup_buttons(self):
+        factory = self.interface.button_factory_f2_5
     def render(self):pass
