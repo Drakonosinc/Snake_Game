a6932b787d5a29546f1dd246a723ec5e7b5f4b61
matias <mat20can@gmail.com>
2025-06-22 16:04:18 -0300

    version 10.0

--- src/Interface/Menus/Main_Menu.py
+++ src/Interface/Menus/Main_Menu.py
@@ -6,4 +6,6 @@ class MainMenu(BaseMenu):
     def setup_buttons(self):
         factory = self.interface.button_factory_f2_5
         self.buttons['play'] = factory.create_TextButton({"text": "PLAY","position": (self.WIDTH/2-60, self.HEIGHT/2-100),"command1":lambda:self.change_mains({"main":2})})
+        self.buttons['quit'] = factory.create_TextButton({"text": "QUIT","position": (self.WIDTH/2-60,self.HEIGHT/2-55),"sound_touch": self.sound_exit,"command1": self.close_game})
+        
     def render(self):pass
