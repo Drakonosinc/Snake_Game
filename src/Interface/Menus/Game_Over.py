a2feaba97c2a7361e12cb30273be29d833733460
matias <mat20can@gmail.com>
2025-06-23 16:05:36 -0300

    version 10.0

--- src/Interface/Menus/Game_Over.py
+++ src/Interface/Menus/Game_Over.py
@@ -3,3 +3,5 @@ class GameOver(BaseMenu):
     def __init__(self, interface):
         super().__init__(interface)
         self.buttons = {}
+    def setup_buttons(self):pass
+    def render(self):pass
