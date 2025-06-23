e046de636867c6a1afdd9bf4680a2d6f6a49ad0d
matias <mat20can@gmail.com>
2025-06-23 16:11:24 -0300

    version 10.0

--- src/Interface/Menus/Game_Over.py
+++ src/Interface/Menus/Game_Over.py
@@ -7,4 +7,4 @@ class GameOver(BaseMenu):
     def render(self):
         self.filt(self.WIDTH,self.HEIGHT,150,self.interface.RED)
         self.screen.blit(self.interface.font3.render("Game Over", True, self.interface.BLACK),(int(self.WIDTH * (52 / 600)),int(self.HEIGHT * (20 / 400 ))))
-        
+        self.execute_buttons(*self.buttons.values())
