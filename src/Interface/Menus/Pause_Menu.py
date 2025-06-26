f344b141b98dba44c7f07811b626d3e5b1459ed4
matias <mat20can@gmail.com>
2025-06-26 15:52:13 -0300

    version 10.0

--- src/Interface/Menus/Pause_Menu.py
+++ src/Interface/Menus/Pause_Menu.py
@@ -6,4 +6,6 @@ class Pause(BaseMenu):
     def setup_buttons(self):
         factory = self.interface.button_factory_f2_5
     def render(self):
+        self.filt(self.WIDTH,self.HEIGHT,150,self.interface.GRAY)
+        self.screen.blit(self.interface.font3.render("Pause", True, "orange"),(int(self.WIDTH * (52 / 600)),int(self.HEIGHT * (20 / 400 ))))
         self.execute_buttons(*self.buttons.values())
