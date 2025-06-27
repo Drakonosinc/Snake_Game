20e5ea6180e187fcaf5e6146d40eed40907ce8b8
matias <mat20can@gmail.com>
2025-06-27 15:48:58 -0300

    version 10.0

--- src/Interface/Interface.py
+++ src/Interface/Interface.py
@@ -48,7 +48,7 @@ class interface(load_elements,BaseMenu):
         self.buttons_sounds()
     def buttons_pausa(self):
         
-        self.option_button = self.button_factory_f2_5.create_TextButton({"text": "Option","position": (50,self.HEIGHT/2-50),"command1":self.reset,"command2":lambda:self.change_mains({"main":4,"run":True}),"command3":self.check_sounds})
+        
         self.menu_button = self.button_factory_f2_5.create_TextButton({"text": "Menu","position": (50,self.HEIGHT/2),"command1":self.reset,"command2":lambda:self.change_mains({"main":0,"run":True}),"command3":self.check_sounds})
         self.exit_button = self.button_factory_f2_5.create_TextButton({"text": "Exit","position": (50,self.HEIGHT/2+50),"sound_touch": self.sound_exit,"command1":self.close_game})
     def menu_options(self):
