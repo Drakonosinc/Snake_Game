6c6238ccd4365e497d8f2a093aa40449440b6d0c
matias <mat20can@gmail.com>
2025-06-27 15:47:11 -0300

    version 10.0

--- src/Interface/Menus/Pause_Menu.py
+++ src/Interface/Menus/Pause_Menu.py
@@ -5,6 +5,7 @@ class Pause(BaseMenu):
         self.buttons = {}
     def setup_buttons(self):
         factory = self.interface.button_factory_f2_5
+        self.buttons['reset'] = factory.create_TextButton({"text": "Reset","position": (50,self.HEIGHT/2-100),"command1":self.interface.reset,"command2":lambda:self.change_mains({"main":-1})})
     def render(self):
         self.filt(self.WIDTH,self.HEIGHT,150,self.interface.GRAY)
         self.screen.blit(self.interface.font3.render("Pause", True, "orange"),(int(self.WIDTH * (52 / 600)),int(self.HEIGHT * (20 / 400 ))))
