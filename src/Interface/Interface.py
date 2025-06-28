3e56632f7e2d3c5a738ef10d045ec499ca7d64b1
matias <mat20can@gmail.com>
2025-06-28 15:05:57 -0300

    version 10.0

--- src/Interface/Interface.py
+++ src/Interface/Interface.py
@@ -31,9 +31,9 @@ class interface(load_elements,BaseMenu):
             1: self.game_over_menu.render,
             2: self.game_mode_menu.render,
             3: self.pause_menu.render,
-            4: self.options_menu.render,}
-        if self.main==5:self.visuals_menu()
-        elif self.main==6:self.keys_menu()
+            4: self.options_menu.render,
+            5: self.visuals_menu.render,}
+        if self.main==6:self.keys_menu()
         elif self.main==7:self.sounds_menu()
         elif self.main==8:self.menu_AI()
         if self.main in menu_routes:menu_routes[self.main]()
