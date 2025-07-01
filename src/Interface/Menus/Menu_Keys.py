8ed0f5e64c76841ad390fbaf41b92ff56e1794e2
matias <mat20can@gmail.com>
2025-07-01 15:56:10 -0300

    version 10.0

--- src/Interface/Menus/Menu_Keys.py
+++ src/Interface/Menus/Menu_Keys.py
@@ -35,5 +35,5 @@ class KeysMenu(BaseMenu):
         if self.key!=None and (self.utils_keys[self.key] and event.type==KEYDOWN):
             self.config.config_keys[self.key]=event.key
             self.config.config_keys[self.key_name]=event.unicode.upper()
-            self.check_item(self.config.config_keys,self.config.config_keys[self.key_name],self.WHITE,"text",**{self.key:self.button_key})
+            self.check_item(self.config.config_keys,self.config.config_keys[self.key_name],self.interface.WHITE,"text",**{self.key:self.button_key})
             self.change_keys(self.key,self.key_name)
