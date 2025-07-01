815ed2b0a5a1d7eed9ba1a7ca2df9b683110e65a
matias <mat20can@gmail.com>
2025-07-01 15:12:53 -0300

    version 10.0

--- src/Interface/Menus/Menu_Keys.py
+++ src/Interface/Menus/Menu_Keys.py
@@ -14,6 +14,7 @@ class KeysMenu(BaseMenu):
         self.buttons_keys['left1_button'] = factory.create_TextButton({"text": self.config.config_keys["Name_key5"],"position": (25,self.HEIGHT/2-50),"command1":lambda:self._change_keys("key_left","Name_key5",self.buttons_keys["left1_button"])})
         self.buttons_keys['right1_button'] = factory.self.button_factory_f2_5.create_TextButton({"text": self.config.config_keys["Name_key7"],"position": (175,self.HEIGHT/2-50),"command1":lambda:self._change_keys("key_right","Name_key7",self.buttons_keys["right1_button"])})
         self.buttons_keys['up2_button'] = factory.create_TextButton({"text": self.config.config_keys["Name_key2"],"position": (self.WIDTH-125,self.HEIGHT/2-100),"command1":lambda:self._change_keys("key_up2","Name_key2",self.buttons_keys["up2_button"])})
+        self.buttons_keys['down2_button'] = factory.create_TextButton({"text": self.config.config_keys["Name_key4"],"position": (self.WIDTH-125,self.HEIGHT/2),"command1":lambda:self._change_keys("key_down2","Name_key4",self.buttons_keys["down2_button"])})
         self.buttons['save_visual'] = factory.create_TextButton({"text": "Save config","position": (self.WIDTH/2,self.HEIGHT-85),"command1":self.config.save_config})
         self.buttons['default_visual'] = factory.create_TextButton({"text": "Default config","position": (self.WIDTH/2-40,self.HEIGHT-50),"command1":lambda:(self.config.config(keys=True),self.change_mains({"main":6,"command":self.setup_buttons}))})
     def render(self):
