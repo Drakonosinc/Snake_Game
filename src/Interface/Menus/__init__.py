5449c99f7cf31799f217c5e14ca1bc0816ba41d1
matias <mat20can@gmail.com>
2025-07-02 16:00:05 -0300

    version 10.0

--- src/Interface/Menus/__init__.py
+++ src/Interface/Menus/__init__.py
@@ -6,5 +6,5 @@ from .Pause_Menu import Pause
 from .Options_Menu import OptionsMenu
 from .Visuals_Menu import VisualsMenu
 from .Menu_Keys import KeysMenu
-# from .Menu_Sounds import SoundsMenu
+from .Menu_Sounds import SoundsMenu
 # from .Menu_AI import AIMenu
